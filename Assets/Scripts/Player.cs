1987a98501a5fafe03feea2867f5d7ba8cb3620b
564094-ahbutcocnut <564094@escg.ac.uk>
2026-04-16 16:06:05 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI healthtext;
+    [SerializeField] float moveSpeed = 6;
+    Animator anim;
+    Rigidbody2D rb;
+
+    
+    int maxHealth = 100;
+    int currentHealth;
+
+    bool dead = false;
+
+    float moveHorizontal, moveVertical;
+    Vector2 movement;
+
+    int facingDirection = 1; // 1 = right, -1 = left
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+        healthtext.text = maxHealth.ToString();
+    }
+
+    private void Update()
+    {
+
+        //Only for testing
+        if(Input.GetKeyDown(KeyCode.J))
+            Hit(10);
+
+
+
+
+
+        if (dead)
+        {
+            movement = Vector2.zero;
+            anim.SetFloat("velocity", 0);
+        }
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(moveHorizontal, moveVertical).normalized;
+
+        anim.SetFloat("velocity", movement.magnitude);
+
+        if (movement.x != 0)
+            facingDirection = movement.x > 0 ? 1 : -1;
+
+        transform.localScale = new Vector2(facingDirection, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check if we collide with an Enemy
+    }
+
+    void Hit(int damage)
+    {
+        anim.SetTrigger("hit");
+        currentHealth -= damage;
+        healthtext.text = Mathf.Clamp(currentHealth, 0, maxHealth).ToString();
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        dead = true;
+        // Call game over
+    }
+
+}
